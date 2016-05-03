1f42bceb79ee1e258351c51f655bc8293525ad36
Oleg Kutsenko <padshyangel@mail.ru>
2016-05-03 23:53:50 +0300

    Removed cors verification

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,9 @@
 class ApiController < ApplicationController
   layout false
   skip_before_action :verify_authenticity_token
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
 
   rescue_from ActiveRecord::RecordNotFound, with: :exception_not_found
 
@@ -8,5 +11,22 @@ class ApiController < ApplicationController
     render json: { errors: ['Not found']}, status: :unprocessable_entity
   end
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 
 end
